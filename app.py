dd012403c70f5ae29264ed5132d64c28902fa287
rockmanzerogx <batsujori@gmail.com>
2025-05-07 14:37:05 -0400

    arreglo de cors version 2

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import os
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/chat": {"origins": "https://rockmanzerogx.github.io"}})
+CORS(app, resources=
+     {r"/chat": {
+         "origins": ["https://rockmanzerogx.github.io","http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:3000"]
+                                
+        }})
 
 API_KEY = os.getenv("DEEPSEEK_API_KEY")
 API_URL = "https://api.deepseek.com/v1/chat/completions"
